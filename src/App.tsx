f76cc07a181da8e9dcc4473367645df52fb38448
Jack Medrek <jmedrek2003@gmail.com>
2024-07-03 16:35:26 -0400

    created About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header';
+import './App.css';
+import About from './components/About';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      Main app!
+      <Header />
+      <About />
     </>
-  )
-}
-
-export default App
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#646cff',
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline>
+        <RouterProvider router={router} />
+      </CssBaseline>
+    </ThemeProvider>
+  </React.StrictMode>
+);
